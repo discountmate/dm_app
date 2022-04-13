73806632db11a48c8befe3a62fcb5fa6dae8aec1
Joel Peterson <jbpe@deakin.edu.au>
2022-04-13 20:01:47 +1000

    Added temp login screen to test login APIs
    Added a temporary login screen to test out login API, will upgrade passwords to be hashed ASAP.

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,88 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const errorController = require('./controllers/error');
+const session = require('express-session');
+const path = require('path');
+const db = require('./util/database');
+const config = require('./config/config.json');
+const mysql = require('mysql2');
 
 //routes
 const shopRoute = require('./routes/shop');
 const itemRoute = require('./routes/item');
+
 //port to listen on
 const ports = process.env.PORT || 3000;
+
+//login connection, need to change to use database.js instead with pool
+const connection = mysql.createConnection({
+  connectionLimit: config.connectionLimit,
+  host: config.host,
+  port: config.port,
+  user: config.user,
+  password: config.password,
+  database: config.database
+});
+
+//login
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+//login modules
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+//login route
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/logintest/login.html'));
+});
+
+//authenticate user
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+//home test
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
 
 //use bodyparser to format into json
 app.use(bodyParser.json());
